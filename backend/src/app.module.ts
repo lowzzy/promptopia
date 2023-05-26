a540fcfdaafd5b3780174f893394a15d41b401b2
lowzzy <60597773+lowzzy@users.noreply.github.com>
2023-05-26 11:59:10 +0900

    使用するcontrollerやserviceの追加

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,13 +2,16 @@ import { Module } from '@nestjs/common';
 import { I18nModule } from './i18n/module';
 
 import { RootController } from './controllers/root.controller';
-import { ItemController } from './controllers/api/v1/item.controller';
+import { ChatController } from './controllers/api/v1/chat.controller';
+import { UserController } from './controllers/api/v1/user.controller';
 import { PrismaService } from './services/prisma/prisma.service';
-import { ItemService } from './services/item/item.service';
+import { ChatService } from './services/chat/chat.service';
+import { DiaryService } from './services/diary/diary.service';
+import { UserService } from './services/user/user.service';
 
 @Module({
   imports: [I18nModule],
-  controllers: [RootController, ItemController],
-  providers: [PrismaService, ItemService],
+  controllers: [RootController, ChatController, UserController],
+  providers: [PrismaService, ChatService, DiaryService, UserService],
 })
 export class AppModule {}
